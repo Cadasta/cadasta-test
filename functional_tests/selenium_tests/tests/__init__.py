287b573726f7b1bb13502de962f02b919b08dd83
manoramahp <manorama@wso2.com>
2016-12-29 14:35:43 +0530

    Registration - test cases for registration attempt with unavailable username/email

--- functional_tests/selenium_tests/tests/__init__.py
+++ functional_tests/selenium_tests/tests/__init__.py
@@ -1,6 +1,8 @@
 from functional_tests.selenium_tests.tests.accounts.registration import (
     NewRegistration,
+    RegistrationAttemptUsernameNotAvailable,
+    RegistrationAttemptEmailNotAvailable
 )
-from functional_tests.selenium_tests.tests.login import (
+from functional_tests.selenium_tests.tests.accounts.login import (
      Login,
 )
--- functional_tests/selenium_tests/tests/accounts/registration.py
+++ functional_tests/selenium_tests/tests/accounts/registration.py
@@ -9,28 +9,65 @@ class NewRegistration(SeleniumTestCase):
     def setUp(self):
         self.wd = CustomWebDriver()
 
-    def tearDown(self):
-        self.wd.quit()
-
     def test_new_registration(self):
         self.open("/dashboard/")
         self.wd.find_element_by_xpath('//a[@href="/account/signup/"]').click()
         self.wd.wait_for_css("#signup_form")
 
-        self.wd.find_css('#id_username').send_keys("user1")
+        self.wd.find_css('#id_username').send_keys("cadasta-test-user1")
         self.wd.find_css('#id_email').send_keys("user1@abc.com")
-        self.wd.find_css("#id_password1").send_keys('user1#qwerty')
-        self.wd.find_css("#id_password2").send_keys('user1#qwerty')
+        self.wd.find_css("#id_password1").send_keys('XYZ#qwerty')
+        self.wd.find_css("#id_password2").send_keys('XYZ#qwerty')
         self.wd.find_element_by_xpath('//button[@name="register"]').click()
 
-        msgs = self.wd.find_element_by_xpath("//div[@id='messages']")
         try:
-            msgs.find_element_by_xpath(
-                ("div[contains(@class,'{}') and contains(.,'{}')]").
-                format('alert', "signed in")
-            )
+            self.wd.find_elements_by_xpath("//*[contains(text(), 'Confirmation email sent to user1@abc.com.')]")
+
         except NoSuchElementException as e:
             # Debugging help.
-            print("Messages: " + msgs.text)
-            # raise e
+            print ("Debug: " + e)
+            self.wd.find_elements_by_xpath("//*[contains(text(), 'A user with that username already exists.')]")
+
+    def tearDown(self):
+        self.wd.quit()
+
+class RegistrationAttemptUsernameNotAvailable(SeleniumTestCase):
+
+    def setUp(self):
+        self.wd = CustomWebDriver()
+
+    def test_registration_attempt_already_taken_username(self):
+        self.open("/dashboard/")
+        self.wd.find_element_by_xpath('//a[@href="/account/signup/"]').click()
+        self.wd.wait_for_css("#signup_form")
+
+        self.wd.find_css('#id_username').send_keys("cadasta-test-user1")
+        self.wd.find_css('#id_email').send_keys("user@abc.com")
+        self.wd.find_css("#id_password1").send_keys('XYZ#qwerty')
+        self.wd.find_css("#id_password2").send_keys('XYZ#qwerty')
+        self.wd.find_element_by_xpath('//button[@name="register"]').click()
+        self.wd.find_elements_by_xpath("//*[contains(text(), 'A user with that username already exists.')]")
+
+    def tearDown(self):
+        self.wd.quit()
+
+class RegistrationAttemptEmailNotAvailable(SeleniumTestCase):
+
+    def setUp(self):
+        self.wd = CustomWebDriver()
+
+    def test_registration_attempt_already_taken_email(self):
+        self.open("/dashboard/")
+        self.wd.find_element_by_xpath('//a[@href="/account/signup/"]').click()
+        self.wd.wait_for_css("#signup_form")
+
+        self.wd.find_css('#id_username').send_keys("user")
+        self.wd.find_css('#id_email').send_keys("user1@abc.com")
+        self.wd.find_css("#id_password1").send_keys('XYZ#qwerty')
+        self.wd.find_css("#id_password2").send_keys('XYZ#qwerty')
+        self.wd.find_element_by_xpath('//button[@name="register"]').click()
+        self.wd.find_elements_by_xpath("//*[contains(text(), 'Another user with this email already exists')]")
+
+def tearDown(self):
+        self.wd.quit()
 
