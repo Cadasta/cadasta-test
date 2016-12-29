50c835c813a4b0ef2aae34192447625a7d29c1a7
manoramahp <manorama@wso2.com>
2016-12-29 13:27:49 +0530

    Deleting unnecessary test case

--- functional_tests/selenium_tests/tests/__init__.py
+++ functional_tests/selenium_tests/tests/__init__.py
@@ -1,10 +1,6 @@
 from functional_tests.selenium_tests.tests.accounts.registration import (
     NewRegistration,
 )
-
-from functional_tests.selenium_tests.tests.auth import (
-    Auth,
-)
 from functional_tests.selenium_tests.tests.login import (
      Login,
 )
