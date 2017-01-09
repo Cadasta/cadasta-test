0f65d207678f8885bff5bc9f1af2e34544333bbf
manoramahp <manorama@wso2.com>
2017-01-09 16:43:20 +0530

    Added organization edit/archive test cases

--- functional_tests/selenium_tests/tests/organizations/organization.py
+++ functional_tests/selenium_tests/tests/organizations/organization.py
@@ -1,7 +1,8 @@
 from functional_tests.selenium_tests.test import SeleniumTestCase
 from functional_tests.selenium_tests.webdriver import CustomWebDriver
+from selenium.common.exceptions import NoSuchElementException
 
-class NewOrganization(SeleniumTestCase):
+class CreateOrganization(SeleniumTestCase):
 
     def setUp(self):
         self.wd = CustomWebDriver()
@@ -25,6 +26,69 @@ class NewOrganization(SeleniumTestCase):
 
         except Exception:
             self.wd.wait_for_css(".error-block")
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+class EditOrganization(SeleniumTestCase):
+
+    def setUp(self):
+        self.wd = CustomWebDriver()
+
+    def test_edit_organization(self):
+        self.user_login()
+        self.wd.wait_for_css('.btn-user')
+        self.open("/organizations/")
+        self.wd.wait_for_xpath("//h1[contains(text(), 'Organizations')]")
+        self.wd.find_element_by_xpath('//a[@href="/organizations/organization-1/"]').click()
+        self.wd.wait_for_xpath("//h2[contains(text(), 'Organization Overview')]")
+        self.open("/organizations/organization-1/edit/")
+        self.wd.wait_for_css(".modal-title")
+        self.wd.find_css('#id_description').clear()
+        self.wd.find_css('#id_description').send_keys("Test organization-1 description edited.")
+        self.wd.find_element_by_xpath('//button[@name="submit"]').click()
+        self.wd.wait_for_xpath("//h2[contains(text(), 'Organization Overview')]")
+        text = self.wd.find_element_by_xpath("//div/section/p").text
+        assert text == "Test organization-1 description edited."
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+class OrganizationArchive(SeleniumTestCase):
+
+    def setUp(self):
+        self.wd = CustomWebDriver()
+
+    def test_archive_organization(self):
+        self.user_login()
+        self.wd.wait_for_css('.btn-user')
+        self.open("/organizations/")
+        self.wd.wait_for_xpath("//h1[contains(text(), 'Organizations')]")
+        self.wd.find_element_by_xpath('//a[@href="/organizations/organization-1/"]').click()
+        self.wd.wait_for_xpath("//h2[contains(text(), 'Organization Overview')]")
+        self.open("/organizations/organization-1/archive/")
+        self.wd.wait_for_xpath("//h2[contains(text(), 'Organization Overview')]")
+        text = self.wd.find_element_by_xpath("//span[contains(@class, 'label-danger')]").text
+        assert text == "ARCHIVED"
+
+    def test_unarchive_organization(self):
+        self.user_login()
+        self.wd.wait_for_css('.btn-user')
+        self.open("/organizations/")
+        self.wd.wait_for_xpath("//h1[contains(text(), 'Organizations')]")
+        self.wd.find_css("#archive-filter").click()
+        self.wd.find_element_by_xpath('//option[@value="All"]').click()
+        self.wd.find_element_by_xpath('//a[@href="/organizations/organization-1/"]').click()
+        self.wd.wait_for_xpath("//h2[contains(text(), 'Organization Overview')]")
+        self.open("/organizations/organization-1/unarchive/")
+        self.wd.wait_for_xpath("//h2[contains(text(), 'Organization Overview')]")
+
+        try:
+            self.wd.find_element_by_xpath("//span[contains(@class, 'label-danger')]")
+        except NoSuchElementException:
+            assert True
 
     def tearDown(self):
         self.wd.quit()
