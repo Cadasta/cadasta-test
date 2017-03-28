e56c2b5b12057b1b78c2003885a6ad1be19445fe
Shaily Sangwan <shaily15297@yahoo.com>
2017-03-29 02:36:25 +0530

    fix projects test suite

--- selenium_tests/projects/location_relationships.py
+++ selenium_tests/projects/location_relationships.py
@@ -20,28 +20,17 @@ class AddLocationRelationship(SeleniumTestCase):
         self.wd.find_element_by_link_text(Project.get_test_proj_name()).click()
         self.wd.wait_for_xpath("//h2[contains(text(), 'Project Overview')]")
         self.wd.find_element_by_link_text("Add location").click()
-
-        self.wd.wait_for_xpath("//h3[contains(text(), 'Draw location on map')]")
-        page_state = self.wd.execute_script('return document.readyState;')
-        while page_state != 'complete':
-            page_state = self.wd.execute_script('return document.readyState;')
-
-        self.wd.find_element_by_css_selector("a.leaflet-draw-draw-marker").click()
+        self.wd.wait_for_css("a.leaflet-draw-draw-marker").click()
 
         action = ActionChains(self.wd)
         elem = self.wd.find_element_by_xpath('//div[@id="id_geometry_map"]')
         action.move_to_element(elem).move_by_offset(15, 15).click().perform()
 
-        page_state = self.wd.execute_script('return document.readyState;')
-        while page_state != 'complete':
-            page_state = self.wd.execute_script('return document.readyState;')
-
         Select(self.wd.find_element_by_id("id_type")).select_by_visible_text("Building")
         self.wd.find_element_by_xpath('//input[@value="Save"]').click()
-        self.wd.wait_for_xpath("//span[contains(text(), 'Location')]")
 
-        self.wd.find_element_by_link_text("Relationships").click()
-        self.wd.find_element_by_link_text("Add relationship").click()
+        self.wd.wait_for_xpath("//*[contains(text(), 'Relationships')]").click()
+        self.wd.wait_for_xpath("//*[contains(text(), 'Add relationship')]").click()
 
         self.wd.switch_to_window(self.wd.window_handles[-1])
         self.wd.wait_for_xpath("//h3[@class='modal-title']")
@@ -62,19 +51,12 @@ class AddLocationRelationship(SeleniumTestCase):
         self.wd.find_element_by_link_text(Project.get_test_proj_name()).click()
         self.wd.wait_for_xpath("//h2[contains(text(), 'Project Overview')]")
 
-        page_state = self.wd.execute_script('return document.readyState;')
-        while page_state != 'complete':
-            page_state = self.wd.execute_script('return document.readyState;')
-
-        action = ActionChains(self.wd)
-        elem = self.wd.find_element_by_xpath('//div[@id="project-map"]')
-        action.move_to_element(elem).perform()
-        self.wd.find_element_by_css_selector('img.leaflet-marker-icon').click()
-        self.wd.find_element_by_link_text("Open location").click()
+        self.wd.wait_for_css('img.leaflet-marker-icon').click()
+        self.wd.wait_for_xpath("//*[contains(text(), 'Open location')]").click()
         self.wd.wait_for_xpath("//span[contains(text(), 'Location')]")
 
-        self.wd.find_element_by_link_text("Relationships").click()
-        self.wd.find_element_by_link_text("Add relationship").click()
+        self.wd.wait_for_xpath("//a[contains(text(),'Relationships')]").click()
+        self.wd.wait_for_xpath("//a[contains(text(),'Add relationship')]").click()
 
         self.wd.switch_to_window(self.wd.window_handles[-1])
         self.wd.wait_for_xpath("//h3[@class='modal-title']")
@@ -104,18 +86,12 @@ class ViewRelationshipDetails(SeleniumTestCase):
         self.wd.find_element_by_link_text(Project.get_test_proj_name()).click()
         self.wd.wait_for_xpath("//h2[contains(text(), 'Project Overview')]")
 
-        page_state = self.wd.execute_script('return document.readyState;')
-        while page_state != 'complete':
-            page_state = self.wd.execute_script('return document.readyState;')
-
-        action = ActionChains(self.wd)
-        elem = self.wd.find_element_by_xpath('//div[@id="project-map"]')
-        action.move_to_element(elem).perform()
+        self.wd.wait_for_css('div.leaflet-marker-icon').click()
+        self.wd.wait_for_css('img.leaflet-marker-icon')
         self.wd.find_element_by_css_selector('img.leaflet-marker-icon').click()
-        self.wd.find_element_by_link_text("Open location").click()
-        self.wd.wait_for_xpath("//span[contains(text(), 'Location')]")
-        self.wd.find_element_by_xpath("//a[contains(text(),'Relationships')]").click()
-        self.wd.find_element_by_xpath("//tr/td/a").click()
+        self.wd.wait_for_xpath("//*[contains(text(), 'Open location')]").click()
+        self.wd.wait_for_xpath("//a[contains(text(),'Relationships')]").click()
+        self.wd.wait_for_xpath("//tr/td/a").click()
         assert self.wd.wait_for_xpath('//*[contains(text(), "Relationship Detail")]')
 
     def tearDown(self):
@@ -134,23 +110,17 @@ class EditRelationshipDetails(SeleniumTestCase):
         self.wd.find_element_by_link_text(Project.get_test_proj_name()).click()
         self.wd.wait_for_xpath("//h2[contains(text(), 'Project Overview')]")
 
-        page_state = self.wd.execute_script('return document.readyState;')
-        while page_state != 'complete':
-            page_state = self.wd.execute_script('return document.readyState;')
-
-        action = ActionChains(self.wd)
-        elem = self.wd.find_element_by_xpath('//div[@id="project-map"]')
-        action.move_to_element(elem).perform()
+        self.wd.wait_for_css('div.leaflet-marker-icon').click()
+        self.wd.wait_for_css('img.leaflet-marker-icon')
         self.wd.find_element_by_css_selector('img.leaflet-marker-icon').click()
-        self.wd.find_element_by_link_text("Open location").click()
-        self.wd.wait_for_xpath("//span[contains(text(), 'Location')]")
-        self.wd.find_element_by_xpath("//a[contains(text(),'Relationships')]").click()
+        self.wd.wait_for_xpath("//*[contains(text(), 'Open location')]").click()
+        self.wd.wait_for_xpath("//a[contains(text(),'Relationships')]").click()
         self.wd.find_element_by_xpath("//tr/td/a").click()
         self.wd.wait_for_xpath('//*[contains(text(), "Relationship Detail")]')
         self.wd.wait_for_xpath('//a[@title="Edit relationship"]').click()
         self.wd.wait_for_xpath('//*[contains(text(), "Edit Relationship")]')
         Select(self.wd.find_element_by_id("id_tenure_type")).select_by_visible_text("Leasehold")
-        self.wd.find_element_by_xpath('//button[@type="submit"]').click()
+        self.wd.wait_for_xpath('//*[contains(text(), "Save")]').click()
         self.wd.wait_for_xpath('//*[contains(text(), "Relationship Detail")]')
         assert self.wd.wait_for_xpath('//*[contains(text(), "Leasehold")]')
 
--- selenium_tests/projects/project_location.py
+++ selenium_tests/projects/project_location.py
@@ -18,21 +18,11 @@ class AddLocation(SeleniumTestCase):
         self.wd.find_element_by_link_text(Project.get_test_proj_name()).click()
         self.wd.wait_for_xpath("//h2[contains(text(), 'Project Overview')]")
         self.wd.find_element_by_link_text("Add location").click()
-        self.wd.wait_for_xpath("//h3[contains(text(), 'Draw location on map')]")
-
-        page_state = self.wd.execute_script('return document.readyState;')
-        while page_state != 'complete':
-            page_state = self.wd.execute_script('return document.readyState;')
-        print page_state
-
         self.wd.find_element_by_xpath('//a[@class="leaflet-draw-draw-marker"]').click()
+
         action = ActionChains(self.wd)
         elem = self.wd.find_element_by_xpath('//div[@id="id_geometry_map"]')
         action.move_to_element(elem).move_by_offset(10, 10).click().perform()
-
-        page_state = self.wd.execute_script('return document.readyState;')
-        while page_state != 'complete':
-            page_state = self.wd.execute_script('return document.readyState;')
 
         Select(self.wd.find_element_by_id("id_type")).select_by_visible_text("Parcel")
         self.wd.find_element_by_xpath('//input[@value="Save"]').click()
@@ -55,19 +45,9 @@ class EditLocation(SeleniumTestCase):
         projects_page.go_to()
 
         self.wd.find_element_by_link_text(Project.get_test_proj_name()).click()
-        self.wd.wait_for_xpath("//h2[contains(text(), 'Project Overview')]")
-
-        page_state = self.wd.execute_script('return document.readyState;')
-        while page_state != 'complete':
-            page_state = self.wd.execute_script('return document.readyState;')
-
-        action = ActionChains(self.wd)
-        elem = self.wd.find_element_by_xpath('//div[@id="project-map"]')
-        action.move_to_element(elem).perform()
-        self.wd.find_element_by_css_selector('img.leaflet-marker-icon').click()
-        self.wd.find_element_by_link_text("Open location").click()
-        self.wd.wait_for_xpath("//span[contains(text(), 'Location')]")
-        self.wd.find_element_by_xpath("//a[@title='Edit location']").click()
+        self.wd.wait_for_css("img.leaflet-marker-icon").click()
+        self.wd.wait_for_xpath('//*[contains(text(), "Open location")]').click()
+        self.wd.wait_for_xpath("//a[@title='Edit location']").click()
         self.wd.wait_for_xpath("//h3[contains(text(), 'Draw location on map')]")
         Select(self.wd.find_element_by_id("id_type")).select_by_visible_text("Apartment")
         self.wd.find_element_by_xpath('//input[@value="Save"]').click()
@@ -90,17 +70,10 @@ class DeleteLocation(SeleniumTestCase):
 
         self.wd.find_element_by_link_text(Project.get_test_proj_name()).click()
         self.wd.wait_for_xpath("//h2[contains(text(), 'Project Overview')]")
-
-        page_state = self.wd.execute_script('return document.readyState;')
-        while page_state != 'complete':
-            page_state = self.wd.execute_script('return document.readyState;')
-
-        action = ActionChains(self.wd)
-        elem = self.wd.find_element_by_xpath('//div[@id="project-map"]')
-        action.move_to_element(elem).perform()
-        self.wd.find_element_by_css_selector('img.leaflet-marker-icon').click()
-        self.wd.find_element_by_link_text("Open location").click()
-        self.wd.wait_for_xpath("//span[contains(text(), 'Location')]")
+        self.wd.wait_for_css("div.leaflet-marker-icon").click()
+        self.wd.wait_for_css("img.leaflet-marker-icon")
+        self.wd.find_element_by_css_selector("img.leaflet-marker-icon").click()
+        self.wd.wait_for_xpath('//*[contains(text(), "Open location")]').click()
         self.wd.find_element_by_xpath("//a[@title='Delete location']").click()
         self.wd.switch_to_window(self.wd.window_handles[-1])
         self.wd.wait_for_xpath("//button[@value='Confirm']").click()
